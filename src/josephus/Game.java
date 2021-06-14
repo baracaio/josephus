d9738681932f332658e13bf2a305a1015ad7c349
Andre-Queiroz <andretorresdequeiroz@gmail.com>
2021-06-13 22:54:14 -0300

    conflict

--- src/josephus/Game.java
+++ src/josephus/Game.java
@@ -1,4 +1,5 @@
 package josephus;
+
 import ds.*;
 
 public class Game implements GameInterface {
@@ -21,10 +22,42 @@ public class Game implements GameInterface {
     }
 
     /**
-     *
-     * @param numberOfPlayers
-     * @param list
-     * @return
+     * @return returns if the game has to continue or not
+     */
+    public boolean playTurn() {
+        int survivors = list.getSize();
+        int counterToKill = 1;
+
+        boolean killedSomeone = false;
+        boolean gameHasToContinue = false;
+
+        NodeInterface current = list.getFirst();
+
+        while (!killedSomeone) {
+            for (int i = 0; i < this.pace; i++) {
+                current = current.getNext();
+                if (current.getValue()) {
+                    counterToKill++;
+                }
+            }
+            if (counterToKill == pace && current.getValue()) {
+                current.setValue(false);
+                survivors--;
+                killedSomeone = true;
+            }
+        }
+
+        if (survivors > 1) {
+            gameHasToContinue = true;
+        }
+
+        return gameHasToContinue;
+    }
+
+    /**
+     * @param numberOfPlayers number of players to be in the circle
+     * @param list            Linked list to be used
+     * @return Informs whether it generated the players or not
      */
     public boolean generatePlayers(int numberOfPlayers, LinkedList list) {
         if (isDataValid(numberOfPlayers, pace)) {
@@ -38,10 +71,9 @@ public class Game implements GameInterface {
     }
 
     /**
-     *
-     * @param numberOfPlayers
-     * @param pace
-     * @return
+     * @param numberOfPlayers number of players to be in the circle
+     * @param pace            key used to count the pace in the circle
+     * @return returns if user input is valid
      */
     public boolean isDataValid(int numberOfPlayers, int pace) {
         return (numberOfPlayers > 1 && numberOfPlayers <= 50) && (pace > 1 && pace <= numberOfPlayers);
